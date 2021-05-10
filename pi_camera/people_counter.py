38ed3b3b94d1dada2e5e25379c984b8fc5dac2fc
SamueleIntaschi <s.intaschi2@studenti.unipi.it>
2021-05-10 14:46:56 +0200

    Removed unnecessary comments

--- pi_camera/people_counter.py
+++ pi_camera/people_counter.py
@@ -1,14 +1,3 @@
-# USAGE
-# To read and write back out to video:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
-#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 \
-#	--output output/output_01.avi
-#
-# To read from webcam and write back out to disk:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
-#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
-#	--output output/webcam_output.avi
-
 # import the necessary packages
 from pyimagesearch.centroidtracker import CentroidTracker
 from pyimagesearch.trackableobject import TrackableObject
